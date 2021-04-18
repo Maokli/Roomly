660978e878015875da3bf8710d527f6611f8ffeb
Maokli2X <louayinfo07@gmail.com>
2021-04-18 16:43:53 +0100

    added real-time caching

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 import { MemberUpdate } from '../models/memberUpdate';
+import "automapper-ts/dist/automapper"
 
 
 @Injectable({
@@ -11,16 +13,22 @@ import { MemberUpdate } from '../models/memberUpdate';
 })
 export class MembersService {
   baseUrl = environment.ApiUrl;
+  members: Member[] = [];
   constructor(private http: HttpClient) { }
 
   getMembers() {
+    if(this.members.length > 0) return of(this.members);
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(map(response => {
       response.forEach(member => member.interestsArray = member.interests.split(','));
+      this.members = response;
       return response;
     }))
   }
 
   getMember(username: string){
+    const member = this.members.find(x => x.userName === username);
+    if(member !== undefined) return of(member)
+    
     return this.http.get<Member>(this.baseUrl + 'users/'+username).pipe(map(response => {
       response.interestsArray = response.interests.split(',')
       return response;
@@ -28,6 +36,17 @@ export class MembersService {
   }
 
   updateMember(member: MemberUpdate) {
-    return this.http.put(this.baseUrl + 'users/',member);
+    return this.http.put(this.baseUrl + 'users/',member).pipe(map(
+      () => {
+        const index = this.members.findIndex(x => x.userName === member.userName);
+        this.members[index] = this.mapToMember(member);
+      }
+    ));
+  }
+
+  private mapToMember(member: MemberUpdate): Member {
+    automapper.createMap('MemberUpdate', 'Member');
+
+    return automapper.map('MemberUpdate', 'Member',member);
   }
 }
