07aa428c4d177071f243467aee2c5a92b7669c72
Maokli2X <louayinfo07@gmail.com>
2021-06-25 11:51:59 +0100

    Added send message functionality

--- client/src/app/app-pages/members/member-messages/member-messages.component.ts
+++ client/src/app/app-pages/members/member-messages/member-messages.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Message } from 'src/app/models/message';
+import { MembersService } from 'src/app/services/members.service';
 import { MessageService } from 'src/app/services/message.service';
 
 @Component({
@@ -9,9 +11,13 @@ import { MessageService } from 'src/app/services/message.service';
   styleUrls: ['./member-messages.component.scss']
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm: NgForm;
   messages: Message[];
+  messageContent: string;
 
-  constructor(private messageService: MessageService, private route: ActivatedRoute) { }
+  constructor(private messageService: MessageService,
+     private route: ActivatedRoute,
+     private memberService: MembersService) { }
 
   ngOnInit(): void {
     this.loadMessagesThread();
@@ -21,5 +27,14 @@ export class MemberMessagesComponent implements OnInit {
     this.messageService
       .getMessageThread(this.route.snapshot.paramMap.get('username'))
       .subscribe(messages => this.messages = messages);
+  }
+
+  sendMessage() {
+    this.messageService
+      .sendMessage(this.route.snapshot.paramMap.get('username'),this.messageContent)
+      .subscribe(message =>{ 
+        this.messages.push(message);
+        this.messageForm.reset();
+      });
   }
 }
